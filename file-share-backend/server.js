d031098ea1df323a1aaeab8ce91515863169558c
PavanKumar1119 <pbmpavankumar11175@gmail.com>
2025-07-08 19:34:41 +0530

    cors

--- file-share-backend/server.js
+++ file-share-backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://quick-share-ui.vercel.app", "http://localhost:5173"],
+    origin: ["https://quickshare-ui.vercel.app", "http://localhost:5173"],
     credentials: true,
   })
 );
